6220e8e3acafba3a9b6cb8d9798ca1b4e7bc320c
hhsl <thmshhsl@gmail.com>
2016-09-19 17:24:15 +0200

    [FEATURE] Map: Display All Data With Correct Type

---
+++ js/dennis.js
@@ -0,0 +1,113 @@
+var dennis = {};
+dennis.map = {
+	styles: [{
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#3498db"
+                }, {
+                    "lightness": 80
+                }]
+            }, {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ecf0f1"
+                }]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }, {
+                    "lightness": 17
+                }]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#ffffff"
+                }, {
+                    "lightness": 29
+                }, {
+                    "weight": 0.2
+                }]
+            }, {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ffffff"
+                }, {
+                    "lightness": 18
+                }]
+            }, {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ffffff"
+                }, {
+                    "lightness": 16
+                }]
+            }, {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ecf0f1"
+                }, {
+                    "lightness": 16
+                }]
+            }, {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#449d44"
+                }, {
+                    "lightness": 80
+                }]
+            }, {
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "visibility": "on"
+                }, {
+                    "color": "#ffffff"
+                }, {
+                    "lightness": 16
+                }]
+            }, {
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#2c3e50"
+                }]
+            }, {
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#f2f2f2"
+                }, {
+                    "lightness": 19
+                }]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#fefefe"
+                }, {
+                    "lightness": 20
+                }]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#fefefe"
+                }, {
+                    "lightness": 17
+                }, {
+                    "weight": 1.2
+                }]
+            }]
+}
--- js/geo.js
+++ js/geo.js
@@ -11,6 +11,7 @@
  */
 
 var marker;
+var map;
 var latitude;
 var longitude;
 
@@ -18,6 +19,7 @@ function initMap() {
 
     var output = document.getElementById("log");
     var locInfo = document.getElementById("locInfo");
+
 
     navigator.geolocation.getCurrentPosition(displayMap, error);
     if (!navigator.geolocation) {
@@ -27,362 +29,42 @@ function initMap() {
 
     function displayMap(position) {
 
-        var tags = [{
-            position: new google.maps.LatLng(53.100983, 8.812796),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100758, 8.812637),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.098392, 8.817197),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.098328, 8.817246),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.098289, 8.817239),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097717, 8.816763),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097240, 8.816620),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097244, 8.816624),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097237, 8.816605),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097233, 8.816615),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097225, 8.816624),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.095222, 8.822699),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.088516, 8.842602),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.082565, 8.853480),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.081116, 8.854983),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.072895, 8.856835),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.065289, 8.853540),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.065235, 8.853518),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.047466, 8.846253),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045418, 8.842974),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045540, 8.843079),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045547, 8.843081),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045971, 8.843265),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045769, 8.844530),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045689, 8.845106),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.045689, 8.845116),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.047245, 8.846125),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.048168, 8.846907),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.064655, 8.853665),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.064728, 8.853714),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.066944, 8.854713),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.070305, 8.856356),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.071171, 8.856645),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.072529, 8.857116),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.076550, 8.855042),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100906, 8.812778),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100914, 8.812786),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100914, 8.812975),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100937, 8.813172),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100937, 8.813267),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100929, 8.813064),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100956, 8.813140),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100891, 8.813365),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100876, 8.813347),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100945, 8.813202),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.101032, 8.813095),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.100773, 8.812794),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.103184, 8.794530),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.102535, 8.785535),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.101109, 8.782074),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.099514, 8.778361),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.098740, 8.776405),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.097935, 8.768972),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.093914, 8.771007),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.093426, 8.770295),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.088337, 8.783664),
-            type: 'tag'
-        }, {
-            position: new google.maps.LatLng(53.102192, 8.784939),
-            type: 'tag'
-        }];
-
-
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         $(document.getElementById("tag")).removeClass("btn-danger").addClass("btn-success");
-        //
-        // locInfo.innerHTML = '<p>Latitude is ' + latitude + ' <br>Longitude is ' + longitude + '</p>';
+
+        // write location inte to dom
         locInfo.innerHTML = latitude + ',' + longitude;
-        //
         output.innerHTML = '';
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 18,
+
+        // initialize map
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
             center: { lat: latitude, lng: longitude },
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles:
-                [{
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#3498db"
-                }, {
-                    "lightness": 80
-                }]
-            }, {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ecf0f1"
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 29
-                }, {
-                    "weight": 0.2
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "road.local",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 16
-                }]
-            }, {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ecf0f1"
-                }, {
-                    "lightness": 16
-                }]
-            }, {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#449d44"
-                }, {
-                    "lightness": 80
-                }]
-            }, {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 16
-                }]
-            }, {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "color": "#2c3e50"
-                }]
-            }, {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f2f2f2"
-                }, {
-                    "lightness": 19
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 20
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 17
-                }, {
-                    "weight": 1.2
-                }]
-            }]
+            styles: dennis.map.styles
         });
 
+        // add current position to map
         marker = new google.maps.Marker({
             map: map,
             draggable: false,
             animation: google.maps.Animation.DROP,
             position: { lat: latitude, lng: longitude }
         });
-        marker.addListener('click', toggleBounce);
-
-        var lat = marker.getPosition().lat();
-
-        console.log(lat);
-        var iconBase = 'images/';
-        var icons = {
-            // based on statistical accident database
-            reference: {
-                icon: iconBase + 'reference_image.png'
-            },
-            // user tags
-            tag: {
-                icon: iconBase + 'tag_image.png'
+        marker.addListener('click', function() {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
             }
-        };
+        });
 
+        // get all marker from json, and add them to the map
+        $.getJSON('/backend/data.json', function(json) {
+            addTagsToMap(json);
+        });
 
-
-        function addMarker(feature) {
-            var marker = new google.maps.Marker({
-                position: feature.position,
-                icon: icons[feature.type].icon,
-                map: map
-            });
-        }
-
-
-
-        for (var i = 0, feature; feature = tags[i]; i++) {
-            addMarker(feature);
-            // console.log(feature);
-        }
     };
 
     function error() {
@@ -391,15 +73,40 @@ function initMap() {
     };
 
     output.innerHTML = "Locating...";
-    //console.log(latitude + ", " + longitude);
 }
 
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+/**
+ * [getIconTypes description]
+ * @return {object} containing all icons
+ */
+function getIconTypes() {
+    // generate some icons
+    var iconBase = 'images/';
+    return {
+        user: {
+            icon: iconBase + 'marker-user_image.png'
+        },
+        authority: {
+            icon: iconBase + 'marker-authority_image.png'
+        }
+    };
+}
+
+/**
+ * This function will add marker to the map with any given
+ * array that has objects with lat and lng attributes
+ * @param {array} tags array of objects with lat and lng
+ */
+function addTagsToMap(tags) {
+    var icons = getIconTypes();
+    _.each(tags, function(tag) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(tag.lat, tag.lng),
+            animation: google.maps.Animation.DROP,
+            icon: icons[tag.type].icon,
+            map: map
+        });
+    });
 }
 
 /**
@@ -411,20 +118,19 @@ function toggleBounce() {
 function tag() {
     var lat = latitude;
     var lng = longitude;
-    var tag = 'tag';
-    var reference = 'reference';
+    var type = 'user';
     var params = [
         'lat=' + lat,
         '&lng=' + lng,
-        '&tag=' + tag,
-        '&reference=' + reference
+        '&type=' + type
     ].join('');
     var request = new XMLHttpRequest();
     request.open('POST', '/backend/save.php', true);
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     request.onload = function() {
         if (this.status >= 200 && this.status < 400) {
-            // Success!
+            addTagsToMap();
+            console.log('worked');
         } else {
             // We reached our target server, but it returned an error
             console.log('server error', this.response);
@@ -434,5 +140,6 @@ function tag() {
         console.log('there was a general error');
     };
     request.send(params);
-
 }
+
+
